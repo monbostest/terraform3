65ae9e0fe0db616f868d50a4dfc0afe9737e3ba4
monbostest <52292370+monbostest@users.noreply.github.com>
2022-06-25 10:28:33 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -1,11 +1,11 @@
 resource "azurerm_resource_group" "example" {
-  name     = "example"
+  name     = "example1"
   location = "West Europe"
   lifecycle {
     prevent_destroy = true
   }
   tags = {
-    "name" = "rg01"
+    "name" = "rg02"
   }
 }
 
